2650504b27ef74c62dc45d46a90394a2b84f3e3b
Indraneel Purohit <i.m.indraneel@gmail.com>
2018-05-03 11:31:03 -0400

    update api endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 // import fetch from 'isomorphic-fetch'
 
-const API_URL = `http://127.0.0.1:5000`;
+const API_URL = `http://162.243.24.164`;
 
 const paramStringBuilder = (obj) => {
   let output = '';
